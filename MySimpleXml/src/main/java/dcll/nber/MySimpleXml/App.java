00e7ed31ea211afe66113d16cc7794816a4fff04
Nathanael Bertrand <cmoinath@gmail.com>
2013-02-21 11:07:18 +0100

    Commit de test

---
+++ MySimpleXml/src/main/java/dcll/nber/MySimpleXml/App.java
@@ -0,0 +1,13 @@
+package dcll.nber.MySimpleXml;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
